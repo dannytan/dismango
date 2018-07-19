8cbe989dbf5917c42c9afdccfa0f5b2387a39a4c
Danny Tan <tandanielm@gmail.com>
2018-07-18 21:45:23 -1000

    Added google cloud api key and request structure

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -12,13 +12,38 @@
   export default {
     data() {
       return {
-        results: null
+        results: null,
+        apiKey: 'AIzaSyAGlHsVmC_099jrau67fglMExpEnuG4Cds',
+        data: {
+          "requests": [{
+            "features": [{
+              "type": null
+            }],
+            "image": {
+              "content": null
+            }
+          }]
+        }
       }
     },
+    methods: {
+        process() {
+          axios
+            .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+            .then(response => (this.results = response.data.bpi));
+
+          /*axios.post(
+            `https://vision.googleapis.com/v1/images:annotate?key=${this.apiKey}`,
+            this.data).then(response => {
+            const result = response.data;
+            console.log(result)
+          }).catch(error => {
+            console.log(error);
+          })*/
+        }
+    },
     mounted () {
-      axios
-        .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-        .then(response => (this.results = response.data.bpi))
+      this.process();
     }
   }
 
