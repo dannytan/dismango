43689d3e3b753508e250756350bf2753824f2b10
Danny Tan <tandanielm@gmail.com>
2018-07-18 23:33:45 -1000

    Vision API call returning data with test image

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,23 +1,38 @@
 <template>
   <v-container fluid>
     <v-layout row>
-      <v-flex x12><pre>{{results}}</pre></v-flex>
+      <v-flex x12><pre>{{data}}</pre></v-flex>
+    </v-layout>
+    <v-layout row>
+
+      <file-base64
+        v-bind:multiple="false"
+        v-bind:done="setImageContent">
+      </file-base64>
+
+    </v-layout>
+    <v-layout row>
+      {{result}}
     </v-layout>
   </v-container>
 </template>
 
 <script>
   import axios from 'axios'
+  import fileBase64 from 'vue-file-base64'
 
   export default {
+    components: {
+        fileBase64
+    },
     data() {
       return {
-        results: null,
+        result: null,
         apiKey: 'AIzaSyAGlHsVmC_099jrau67fglMExpEnuG4Cds',
         data: {
           "requests": [{
             "features": [{
-              "type": null
+              "type": 'DOCUMENT_TEXT_DETECTION'
             }],
             "image": {
               "content": null
@@ -28,22 +43,19 @@
     },
     methods: {
         process() {
-          axios
-            .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-            .then(response => (this.results = response.data.bpi));
-
-          /*axios.post(
+          axios.post(
             `https://vision.googleapis.com/v1/images:annotate?key=${this.apiKey}`,
             this.data).then(response => {
-            const result = response.data;
-            console.log(result)
+            this.result = response.data.responses[0].textAnnotations[0].description;
           }).catch(error => {
-            console.log(error);
-          })*/
-        }
-    },
-    mounted () {
-      this.process();
+            console.error(error);
+          })
+        },
+        setImageContent(file) {
+            let base64 = file.base64;
+            this.data.requests[0].image.content = base64.replace("data:image/png;base64,", "");
+            this.process();
+        },
     }
   }
 
