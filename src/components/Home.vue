06e7f183b37cbfc659ca8684413ebadc9870f2e8
Danny Tan <tandanielm@gmail.com>
2018-07-18 20:30:36 -1000

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container fluid>
+    <v-layout row>
+      <v-flex x12>Home</v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script type="text/ecmascript-6">
+
+  export default {
+    data() {
+      return {}
+    }
+  };
+
+</script>
+
+<style scoped type="text/css" lang="css"></style>
+Hello
